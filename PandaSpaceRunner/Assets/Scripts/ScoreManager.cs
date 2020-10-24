1c136b8b01ef18197e976abe065a2303e8e338f0
James Shamy <jamesshamy@gmail.com>
2020-10-23 23:39:55 -0600

    Beta 1

---
+++ PandaSpaceRunner/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static ScoreManager instance;
+    public int score;
+    public int highScore;
+    public int totalDiamonds;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        totalDiamonds = PlayerPrefs.GetInt("totalDiamonds", 0);
+        PlayerPrefs.SetInt("score", score);
+        
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+    public void diamondCount()
+    {
+        score += 1;
+        totalDiamonds += 1;
+        PlayerPrefs.SetInt("totalDiamonds", totalDiamonds);
+        PlayerPrefs.Save();
+        
+       
+    }
+
+    public void incrementScore()
+    {
+        score += 1;
+    }
+
+    public void startScore()
+    {
+        InvokeRepeating("incrementScore", 0.1f, 1f);
+        
+    }
+
+    public void StopScore()
+    {
+        CancelInvoke("incrementScore");
+        PlayerPrefs.SetInt("score", score);
+        if(PlayerPrefs.HasKey("highScore"))
+        {
+            if (score > PlayerPrefs.GetInt("highScore"))
+            {
+                PlayerPrefs.SetInt("highScore", score);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("highScore",score);
+        }
+    }
+
+}
---
+++ PandaSpaceRunner/Assets/Scripts/UiManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+    public static UiManager instance;
+
+    
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public GameObject diamondCountPanel;
+    public Text diamondCount;
+    public Text score;
+    public Text highScore;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+       
+    }
+
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        diamondCountPanel.SetActive(true);
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        diamondCount.text = PlayerPrefs.GetInt("totalDiamonds").ToString();
+
+    }
+
+    public void GameOver()
+    {
+
+        ScoreManager.instance.StopScore();
+        score.text = PlayerPrefs.GetInt ("score").ToString();
+        highScore.text = PlayerPrefs.GetInt("highScore").ToString();
+        gameOverPanel.SetActive(true);
+        diamondCountPanel.SetActive(false);
+        
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        diamondCount.text = PlayerPrefs.GetInt("totalDiamonds").ToString();
+
+    }
+}
