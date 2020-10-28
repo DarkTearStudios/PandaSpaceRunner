a0b1f9867a8fc0d8413a284ae3fc095506898b72
James Shamy <jamesshamy@gmail.com>
2020-10-28 17:16:15 -0600

    Beta 3.5

--- PandaSpaceRunner/Assets/Scripts/BallController.cs
+++ PandaSpaceRunner/Assets/Scripts/BallController.cs
@@ -1,184 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class BallController : MonoBehaviour
 {
 
-	private Vector2 fingerDownPos;
-	private Vector2 fingerUpPos;
-	public GameObject particle;
-	Animator animator;
-	[SerializeField]
-	private float speed;
-	bool started;
-	bool gameOver;
-	
-	
-
-	Rigidbody rb;
-
-	public bool detectSwipeAfterRelease = false;
-
-	public float SWIPE_THRESHOLD = 20f;
+    public GameObject particle;
+    public GameObject GreenParticle;
+    Animator animator;
+    [SerializeField]
+    private float speed;
+    bool started;
+    bool gameOver;
+    bool running;
+    Rigidbody rb;
 
 
-	private void Awake()
-	{
-		rb = GetComponent<Rigidbody>();
-		animator = GetComponent<Animator>();
-		
-	}
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
 
-	// Use this for initialization
-	void Start()
-	{
+    // Use this for initialization
+    void Start()
+    {
 
-		started = false;
-		gameOver = false;
-		speed = 8f;
-		
-	}
+        started = false;
+        gameOver = false;
+        running = false;
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (!started)
-		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				rb.velocity = new Vector3(0, 0, speed);
-				started = true;
-				animator.SetLayerWeight(animator.GetLayerIndex("RunForward"), 1f);
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+                animator.SetLayerWeight(animator.GetLayerIndex("RunForward"), 1f);
+                
 
-				GameManager.instance.StartGame();
-			}
-		}
+                GameManager.instance.StartGame();
+            }
+        }
 
-		if (!Physics.Raycast(transform.position, Vector3.down, 1f))
-		{
-			gameOver = true;
-			rb.velocity = new Vector3(0, -25f, 0);
+        //Debug.DrawRay(transform.position, Vector3.down, Color.red);
 
-			Camera.main.GetComponent<CameraFollow>().gameOver = true;
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            rb.velocity = new Vector3(0, -25f, 0);
 
-			GameManager.instance.GameOver();
-		}
+            Camera.main.GetComponent<CameraFollow>().gameOver = true;
 
-		foreach (Touch touch in Input.touches)
-		{
-			if (touch.phase == TouchPhase.Began)
-			{
-				fingerUpPos = touch.position;
-				fingerDownPos = touch.position;
-			}
+            GameManager.instance.GameOver();
+        }
 
-			//Detects Swipe while finger is still moving on screen
-			if (touch.phase == TouchPhase.Moved)
-			{
-				if (!detectSwipeAfterRelease)
-				{
-					fingerDownPos = touch.position;
-					DetectSwipe();
-					
-				}
-			}
+        if (Input.GetMouseButtonDown(0) && !gameOver)
+        {
+            SwitchDirection();
+        }
+    }
 
-			//Detects swipe after finger is released from screen
-			if (touch.phase == TouchPhase.Ended)
-			{
-				fingerDownPos = touch.position;
-				DetectSwipe();
-				
-			}
-		}
+    void SwitchDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+            transform.Rotate(0, 90, 0);
+        }
 
-		
-	}
+        else if (rb.velocity.x > 0 && !running)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+            running = true;
+        }
 
-	
+        else if (rb.velocity.x > 0 && running)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+            transform.Rotate(0, -90, 0);
+        }
+    }
 
-	void DetectSwipe()
-	{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Diamond")
+        {
+            GameObject part = Instantiate(particle, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(part, 1f);
+            ScoreManager.instance.diamondCount();
 
-		if (VerticalMoveValue() > SWIPE_THRESHOLD && VerticalMoveValue() > HorizontalMoveValue())
-		{
-			Debug.Log("Vertical Swipe Detected!");
-			if (fingerDownPos.y - fingerUpPos.y > 0)
-			{
-				OnSwipeUp();
-			}
-			else if (fingerDownPos.y - fingerUpPos.y < 0)
-			{
-				OnSwipeDown();
-			}
-			fingerUpPos = fingerDownPos;
+        }
 
-		}
-		else if (HorizontalMoveValue() > SWIPE_THRESHOLD && HorizontalMoveValue() > VerticalMoveValue())
-		{
-			Debug.Log("Horizontal Swipe Detected!");
-			if (fingerDownPos.x - fingerUpPos.x > 0)
-			{
-				OnSwipeRight();
-			}
-			else if (fingerDownPos.x - fingerUpPos.x < 0)
-			{
-				OnSwipeLeft();
-			}
-			fingerUpPos = fingerDownPos;
-
-		}
-		else
-		{
-			Debug.Log("No Swipe Detected!");
-		}
-	}
-
-	float VerticalMoveValue()
-	{
-		return Mathf.Abs(fingerDownPos.y - fingerUpPos.y);
-	}
-
-	float HorizontalMoveValue()
-	{
-		return Mathf.Abs(fingerDownPos.x - fingerUpPos.x);
-	}
-
-	void OnSwipeUp()
-	{
-		
-	}
-
-	void OnSwipeDown()
-	{
-		//Do something when swiped down
-	}
-
-	void OnSwipeLeft()
-	{
-		rb.velocity = new Vector3(0, 0, speed);
-		animator.SetLayerWeight(animator.GetLayerIndex("RunRight"), 0f);
-	}
-
-	void OnSwipeRight()
-	{
-		rb.velocity = new Vector3(speed, 0, 0);
-		animator.SetLayerWeight(animator.GetLayerIndex("RunRight"), 1f);
-	}
-
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.tag == "Diamond")
-		{
-			GameObject part = Instantiate(particle, other.gameObject.transform.position, Quaternion.identity);
-			Destroy(other.gameObject);
-			Destroy(part, 1f);
-			ScoreManager.instance.diamondCount();
-
-		}
-
-		
-	}
+        
+    }
 }
