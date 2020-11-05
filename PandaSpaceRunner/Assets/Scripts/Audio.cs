c96de1cdf185ff2c2696039a6a708a7cbaee8217
James Shamy <jamesshamy@gmail.com>
2020-11-05 03:25:29 -0700

    Removed uneeded Code. Scripts, and Assets

--- PandaSpaceRunner/Assets/Scripts/Audio.cs
+++ PandaSpaceRunner/Assets/Scripts/Audio.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Audio : MonoBehaviour
 {
+    public int Mute;
+    public int MuteFX;
+
+    public Toggle MuterSwitch;
+    public Toggle MuterFXSwitch;
 
     private static Audio instance = null;
     
@@ -27,13 +34,55 @@ public class Audio : MonoBehaviour
         {
             instance = this;
         }
-        
+
+        Mute = PlayerPrefs.GetInt("Mute");
+        MuteFX = PlayerPrefs.GetInt("MuteFX");
+
     }
     
     void Start()
     {
         i = Random.Range(0, musicbg.Length);
         StartCoroutine("Playlist");
+
+        PlayerPrefs.SetInt("Mute", Mute);
+        if (Mute == 1)
+        {
+            MuterSwitch.isOn = true;
+        }
+
+        PlayerPrefs.SetInt("MuteFX", MuteFX);
+        if (MuteFX == 1)
+        {
+            MuterFXSwitch.isOn = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (MuterSwitch.isOn == false)
+        {
+            Mute = 0;
+            PlayerPrefs.SetInt("Mute", Mute);
+        }
+
+        if (MuterFXSwitch.isOn == false)
+        {
+            MuteFX = 0;
+            PlayerPrefs.SetInt("MuteFX", MuteFX);
+        }
+    }
+
+    public void Muter()
+    {
+        Mute = 1;
+        PlayerPrefs.SetInt("Mute", Mute);
+    }
+
+    public void MuterFX()
+    {
+        MuteFX = 1;
+        PlayerPrefs.SetInt("MuteFX", MuteFX);
     }
 
     IEnumerator Playlist()
---
+++ PandaSpaceRunner/Assets/Scripts/BuyChecker.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyChecker : MonoBehaviour
+{
+    public GameObject[] BuyCheckButton;
+    public GameObject[] DiamondsCost;
+    public Toggle[] Toggle;
+    public int BuyCheck;
+    public int TotalDiamonds;
+
+
+    private void Awake()
+    {
+        TotalDiamonds = PlayerPrefs.GetInt("totalDiamonds");
+        BuyCheck = PlayerPrefs.GetInt("BuyCheck0");
+    }
+    
+
+    public void Buy1()
+    {
+        if (TotalDiamonds >= 1000)
+        {
+            TotalDiamonds = TotalDiamonds - 1000;
+            PlayerPrefs.SetInt("totalDiamonds", TotalDiamonds);
+            BuyCheckButton[0].SetActive(false);
+            Toggle[0].interactable = true;
+            BuyCheck = 1;
+            PlayerPrefs.SetInt("BuyCheck0", BuyCheck);
+            DiamondsCost[0].SetActive(false);
+            UiManager.instance.GemCheckClose();
+            Toggle[0].isOn = true;
+        }
+
+        else
+        {
+            
+            
+            UiManager.instance.GemCheckClose();
+
+            UiManager.instance.NotEnoughOpen();
+        }
+    }
+
+    public void Buy2()
+    {
+        if (TotalDiamonds >= 1000)
+        {
+            TotalDiamonds = TotalDiamonds - 1000;
+            PlayerPrefs.SetInt("totalDiamonds", TotalDiamonds);
+            BuyCheckButton[1].SetActive(false);
+            Toggle[1].interactable = true;
+            BuyCheck = 1;
+            PlayerPrefs.SetInt("BuyCheck1", BuyCheck);
+            DiamondsCost[1].SetActive(false);
+            UiManager.instance.GemCheckClose2();
+            Toggle[1].isOn = true;
+        }
+
+        else
+        {
+            Debug.Log("Not Engough Gems");
+
+            UiManager.instance.GemCheckClose2();
+
+            UiManager.instance.NotEnoughOpen();
+
+        }
+    }
+
+    public void Buy3()
+    {
+        if (TotalDiamonds >= 1000)
+        {
+            TotalDiamonds = TotalDiamonds - 1000;
+            PlayerPrefs.SetInt("totalDiamonds", TotalDiamonds);
+            BuyCheckButton[2].SetActive(false);
+            Toggle[2].interactable = true;
+            BuyCheck = 1;
+            PlayerPrefs.SetInt("BuyCheck2", BuyCheck);
+            DiamondsCost[2].SetActive(false);
+            UiManager.instance.GemCheckClose3();
+            Toggle[2].isOn = true;
+        }
+
+        else
+        {
+            Debug.Log("Not Engough Gems");
+
+            UiManager.instance.GemCheckClose3();
+
+            UiManager.instance.NotEnoughOpen();
+        }
+    }
+
+    public void Buy4()
+    {
+        if (TotalDiamonds >= 1000)
+        {
+            TotalDiamonds = TotalDiamonds - 1000;
+            PlayerPrefs.SetInt("totalDiamonds", TotalDiamonds);
+            BuyCheckButton[3].SetActive(false);
+            Toggle[3].interactable = true;
+            BuyCheck = 1;
+            PlayerPrefs.SetInt("BuyCheck3", BuyCheck);
+            DiamondsCost[3].SetActive(false);
+            UiManager.instance.GemCheckClose4();
+            Toggle[3].isOn = true;
+        }
+
+        else
+        {
+            Debug.Log("Not Engough Gems");
+
+            UiManager.instance.GemCheckClose4();
+
+            UiManager.instance.NotEnoughOpen();
+        }
+    }
+
+    public void Buy5()
+    {
+        if (TotalDiamonds >= 1000)
+        {
+            TotalDiamonds = TotalDiamonds - 1000;
+            PlayerPrefs.SetInt("totalDiamonds", TotalDiamonds);
+            BuyCheckButton[4].SetActive(false);
+            Toggle[4].interactable = true;
+            BuyCheck = 1;
+            PlayerPrefs.SetInt("BuyCheck4", BuyCheck);
+            DiamondsCost[4].SetActive(false);
+            UiManager.instance.GemCheckClose5();
+            Toggle[4].isOn = true;
+        }
+
+        else
+        {
+            Debug.Log("Not Engough Gems");
+
+            UiManager.instance.GemCheckClose5();
+
+            UiManager.instance.NotEnoughOpen();
+        }
+    }
+}
